3d6bc8ad09fe09d004bfcd4f3d0d30809ef6ae64
owboson <115831817+owboson@users.noreply.github.com>
2023-07-12 23:08:31 +0200

    [Moodivation] adapt UI and reminder notification

--- Moodivation/app/src/main/java/de/b08/moodivation/notifications/NotificationReceiver.java
+++ Moodivation/app/src/main/java/de/b08/moodivation/notifications/NotificationReceiver.java
@@ -2,35 +2,47 @@ package de.b08.moodivation.notifications;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 import androidx.core.app.NotificationCompat;
 
+import de.b08.moodivation.QuestionnaireActivity;
 import de.b08.moodivation.R;
 public class NotificationReceiver extends BroadcastReceiver {
     Notification notification;
     @Override
     public void onReceive(Context context, Intent intent) {
+        Intent questionnaireIntent = new Intent(context, QuestionnaireActivity.class);
+        questionnaireIntent.putExtra("name", "main");
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, questionnaireIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
         String action = intent.getAction();
         if (action.equals("morningId")) {
             notification = new NotificationCompat.Builder(context, "channelId")
                     .setContentTitle(context.getResources().getString(R.string.questionnaireNotificationTitle))
                     .setContentText(context.getResources().getString(R.string.morningQuestionnaireNotification))
-                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setSmallIcon(R.drawable.moodivation_icon_foreground)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
                     .build();
         } else if (action.equals("dayId")) {
             notification = new NotificationCompat.Builder(context, "channelId")
                     .setContentTitle(context.getResources().getString(R.string.questionnaireNotificationTitle))
                     .setContentText(context.getResources().getString(R.string.noonQuestionnaireNotification))
-                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setSmallIcon(R.drawable.moodivation_icon_foreground)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
                     .build();
         } else if (action.equals("eveningId")) {
             notification = new NotificationCompat.Builder(context, "channelId")
                     .setContentTitle(context.getResources().getString(R.string.questionnaireNotificationTitle))
                     .setContentText(context.getResources().getString(R.string.eveningQuestionnaireNotification))
-                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setSmallIcon(R.drawable.moodivation_icon_foreground)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
                     .build();
         }
         System.out.println(notification);
